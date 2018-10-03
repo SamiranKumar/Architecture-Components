e5ea405b32320ac47c8c4a9ee19f55644345396f
SamiranKumar <samirankumar11@gmail.com>
2018-10-03 21:14:57 +0600

    implement recycleview

--- app/src/main/java/com/skh/architecturecomponentsdemo/MainActivity.java
+++ app/src/main/java/com/skh/architecturecomponentsdemo/MainActivity.java
@@ -6,23 +6,41 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
     private NoteViewmodel noteViewmodel;
+
+    @BindView(R.id.rv_note_list)
+    RecyclerView rv_note_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+
+        rv_note_list.setLayoutManager(new LinearLayoutManager(this));
+        rv_note_list.setHasFixedSize(true);
+
+        final NoteAdapter noteAdapter = new NoteAdapter();
+        rv_note_list.setAdapter(noteAdapter);
+
+
         noteViewmodel = ViewModelProviders.of(this).get(NoteViewmodel.class);
         noteViewmodel.getListLiveData().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(@Nullable List<Note> notes) {
-
+                noteAdapter.setNotes(notes);
             }
         });
 
---
+++ app/src/main/java/com/skh/architecturecomponentsdemo/NoteAdapter.java
@@ -0,0 +1,71 @@
+package com.skh.architecturecomponentsdemo;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+    private List<Note> notes;
+
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.row_note_item, viewGroup, false);
+        return new NoteHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder noteHolder, int i) {
+
+        Note note = notes.get(i);
+        noteHolder.bind(note);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return notes != null ? notes.size() : 0;
+    }
+
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.tv_tittle)
+        TextView tv_tittle;
+
+        @BindView(R.id.tv_description)
+        TextView tv_description;
+
+        @BindView(R.id.tv_priority)
+        TextView tv_priority;
+        @BindView(R.id.tv_id)
+        TextView tv_id;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ButterKnife.bind(this, itemView);
+        }
+
+
+        public void bind(Note note) {
+            tv_tittle.setText(note.getTittle());
+            tv_priority.setText(note.getPriority());
+            tv_description.setText(note.getDescription());
+            tv_id.setText(note.getId());
+        }
+    }
+}
